faaea31abf85e956ac8f2dd90e18d2a0c3a65929
ysrann <faiqyusran@gmail.com>
2025-05-16 09:07:05 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Game Mencocokkan Gambar</title>
+  <link rel="stylesheet" href="style.css" />
+</head>
+<body>
+  <h1>Cocokkan Gambar!</h1>
+  <div class="game-board" id="gameBoard"></div>
+  <script src="script.js"></script>
+</body>
+</html>
